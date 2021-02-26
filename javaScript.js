7facc3268c4214df8b7558886ccf14d800b88a8d
Andre Rodriguez <drae7299@gmail.com>
2021-02-26 00:07:04 -0500

    added in HW index to html file

---
+++ javaScript.js
@@ -0,0 +1,11 @@
+//global Vars
+//Comparison fuhnction
+// "This" functiion to save data
+//Click button, save value then place it into an array and stringify it
+
+// each element in the array is an object
+//put into array that you stringify
+//store array in local storage
+//parse it when you bring it back out
+//have to stringify to save
+//got to parse to place on the page appropriatly 
